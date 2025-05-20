1c6640c6a3edfc0d3c611ad1acaedb52fcf6b8fd
DoroDore <tlyz82@student.cis.eduhk>
2025-05-20 08:41:40 +0800

    Initial Commit

---
+++ WeatherForecastTests/WeatherForecastTests.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherForecastTests.swift
+//  WeatherForecastTests
+//
+//  Created by Theodore Zhu on 5/20/25.
+//
+
+import Testing
+
+struct WeatherForecastTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
